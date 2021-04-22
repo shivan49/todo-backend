072684d0fa441cd4da1a72fe7b5e1d599ecd0299
Arpan Sardar <arpanspeaks@gmail.com>
2021-04-22 13:20:53 +0530

    first-commit

---
+++ src/test/java/com/example/fullstackangularspringboot/FullstackAngularSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackangularspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAngularSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
